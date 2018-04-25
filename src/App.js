c0bc2e7baba06bee70e6259920c70364a8ec041d
Daktari <dindijjames@gmail.com>
2018-04-25 12:38:08 +0000

    Validating PropTypes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
+
+// Props
+/*
 class App extends React.Component {
   constructor(props){
     super(props);
@@ -20,5 +25,39 @@ class App extends React.Component {
 App.defaultProps = {
   headerProp: "Header from prop ...",
   contentProp: "Content from prop ..."
+}*/
+
+// Validating props
+/**/
+class App extends Component {
+  render(){
+    return(
+      <div>
+        <h1>Hello, {this.props.name}</h1>
+        <h3>Array: {this.props.propArray}</h3>
+        <h3>Bool: {this.props.propBool ? "True ..." : "False ..."}</h3>
+        <h3>Func: {this.props.propFunc(3)}</h3>
+        <h3>Number: {this.props.propNumber}</h3>
+        <h3>String: {this.props.propString}</h3>
+      </div>
+      );
+  }
 }
+App.propTypes = {
+    name: PropTypes.string,
+    propArray: PropTypes.array.isRequired,
+    propBool: PropTypes.bool.isRequired,
+    propFunc: PropTypes.func,
+    propNumber: PropTypes.number,
+    propString: PropTypes.string,
+  };
+  App.defaultProps = {
+    name: 'Tutorialspoint.com',
+    propArray: [1, 2, 3, 4, 5],
+    propBool: true,
+    propFunc: (e) => {return e},
+    propNumber: 1,
+    propString: 'String value ...'
+  }
+
 export default App;
